96db5d7091c5f2de969dbb92f56d27e5fed88d38
Manousos Sgourinakis <msgourinakis@gmail.com>
2026-05-31 15:30:36 +0300

    add OIDC setup and bootstrap workflow

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -7,3 +7,8 @@ output "state_bucket_arn" {
   description = "ARN of S3 bucket"
   value       = aws_s3_bucket.terraform_state.arn
 }
+
+output "github_actions_role_arn" {
+  description = "ARN of IAM Role for GitHub Actions"
+  value       = aws_iam_role.github_actions.arn
+}
--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,3 +1,7 @@
+###########################
+## PROVIDER  / S3 BUCKET ##
+###########################
+
 variable "aws_region" {
   description = "AWS region"
   type        = string
@@ -7,4 +11,19 @@ variable "aws_region" {
 variable "state_bucket_name" {
   description = "S3 bucket name for Terraform state"
   type        = string
+}
+
+####################
+## BOOTSTRAP OIDC ##
+####################
+
+variable "github_username" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "secure-aws-iac"
 }
