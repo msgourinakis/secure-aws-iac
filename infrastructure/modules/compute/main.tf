3b96fbf76549ace5a8b83a533bd2ea6ff7cd6c98
Manousos Sgourinakis <msgourinakis@gmail.com>
2026-06-16 11:33:54 +0300

    added asg health check grace period

--- infrastructure/modules/compute/main.tf
+++ infrastructure/modules/compute/main.tf
@@ -60,6 +60,7 @@ resource "aws_autoscaling_group" "app" {
   vpc_zone_identifier = var.private_subnet_ids
   target_group_arns   = [aws_lb_target_group.app.arn]
   health_check_type   = "ELB"
+  health_check_grace_period = 600
 
   min_size         = 1
   max_size         = 2
