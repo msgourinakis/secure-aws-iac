151130406c0b963ecbed821090132f42d3f6b482
Manousos Sgourinakis <msgourinakis@gmail.com>
2026-06-16 16:47:04 +0300

    changed asg settings

--- infrastructure/modules/compute/main.tf
+++ infrastructure/modules/compute/main.tf
@@ -70,11 +70,11 @@ resource "aws_autoscaling_group" "app" {
   vpc_zone_identifier = var.private_subnet_ids
   target_group_arns   = [aws_lb_target_group.app.arn]
   health_check_type   = "ELB"
-  health_check_grace_period = 600
+  health_check_grace_period = 1200
 
-  min_size         = 1
-  max_size         = 2
-  desired_capacity = 1
+  min_size         = 2
+  max_size         = 3
+  desired_capacity = 2
 
   launch_template {
     id      = aws_launch_template.app.id
