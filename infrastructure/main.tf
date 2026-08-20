79886d80b332d4064e7d91e6fa74f55e4cfdc9ff
Manousos Sgourinakis <msgourinakis@gmail.com>
2026-08-20 19:12:27 +0300

    added: local provider

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 
   backend "s3" {
